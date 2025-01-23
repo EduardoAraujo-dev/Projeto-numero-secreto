153678f93a1f2b002d8ba9ce960e7797a03c0f9d
Eduardo de Araujo <edgyaraujo@gmail.com>
2025-01-23 09:03:58 -0300

    deixando o jogo um pouco mais hard

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumeroSorteados =[];
-let numeroLimite = 100;
+let numeroLimite = 110;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1; 
 
@@ -20,13 +20,13 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 110');
 }
 
 exibirMensagemInicial();
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 110');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
