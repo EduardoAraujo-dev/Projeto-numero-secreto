c7261802c873b502753578aa4174334986edc70a
Eduardo de Araujo <edgyaraujo@gmail.com>
2025-01-30 08:35:32 -0300

    alteração no código

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 110');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 120');
 }
 
 exibirMensagemInicial();
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 110');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 120');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
